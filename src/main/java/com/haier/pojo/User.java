e339b0a09e155292640633897757e6a3dfdc84d7
haier <bos@haier.cn>
2022-03-05 17:46:37 +0800

    初始化

---
+++ src/main/java/com/haier/pojo/User.java
@@ -0,0 +1,22 @@
+/**
+ * 一些声明信息
+ * Description:
+ * date: 2022/3/5 17:33
+ *
+ * @author Haier
+ * @version 1.0
+ * @since JDK 1.8
+ */
+package com.haier.pojo;
+
+/**
+ * 一些声明信息
+ * Description:
+ * date: 2022/3/5 17:33
+ * @author Haier
+ * @version 1.0
+ * @since JDK 1.8
+ */
+public class User {
+    private String name;
+}
